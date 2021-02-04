84ed11acce767c85ebca32f2285946cdc0b9f9f6
Prem Dahal <Prem.dahala01@gmail.com>
2021-02-04 00:14:56 -0500

    final commit

--- app.py
+++ app.py
@@ -16,15 +16,16 @@ toolbar = DebugToolbarExtension(app)
 
 @app.route('/')
 def home_page():
+    """ Simply redirects to register page"""
     return redirect('/register')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
-
+    """ Allows a user to register if condition meets else redirected to registration page """
+    
     form = RegisterForm()
 
     if form.validate_on_submit():
-
         first_name = form.first_name.data
         last_name = form.last_name.data
         email = form.email.data
@@ -42,21 +43,10 @@ def register_page():
     else:
         return render_template('register.html', form=form)
 
-@app.route('/users/<username>')
-def secret_page(username):
-
-    if 'username' not in session:
-        return redirect('/login')
-
-
-    user = User.query.filter_by(username=username).first()
-    feedbacks = Feedback.query.filter_by(username=username)
-
-    return render_template('secret.html', user=user, feedbacks=feedbacks)
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_user():
+    ''' Authenticates and logs the user in'''
 
     form = LoginForm()
 
@@ -69,34 +59,46 @@ def login_user():
             flash(f'Greetings {user.first_name}!')
             session['username'] = user.username
             return redirect(f'/users/{user.username}')
-
         else:
             form.username.errors = ['Invalid Username/password']
-        
     return render_template('login.html', form=form)
 
 
-#its better to make a logout a post request. Just add an empty form
-@app.route('/logout', methods=['GET', 'POST'])
+@app.route('/users/<username>')
+def secret_page(username):
+    """ Displays information regarding the user and their feeds. Allows to create new as well"""
+
+    if 'username' not in session:
+        return redirect('/login')
+
+    user = User.query.filter_by(username=username).first()
+    feedbacks = Feedback.query.filter_by(username=username)
+
+    return render_template('secret.html', user=user, feedbacks=feedbacks)
+
+
+
+@app.route('/logout', methods=['POST'])
 def logout_user():
+    """ Logs out the user"""
 
     flash(f'You been logged out')
     session.pop('username')
     return redirect('/login')
 
+
 @app.route('/users/<username>/feedbacks/add', methods=['GET', 'POST'])
 def add_feedback(username):
+    """ Allows the user to add new feedbacks"""
 
     if 'username' not in session:
         return redirect('/login')
     
-    # user = User.query.filter_by(username=username).first()
     form = FeedbackForm()
 
     if form.validate_on_submit():
         title = form.title.data
         content = form.content.data
-
         feed = Feedback(title=title, content=content, username=username)
         db.session.add(feed)
         db.session.commit()
@@ -104,3 +106,58 @@ def add_feedback(username):
         return redirect(f'/users/{username}')
 
     return render_template('feedback.html', form=form)
+
+
+@app.route('/feedback/<int:id>/update', methods=['GET', 'POST'])
+def update_feedback(id):
+    """ Allows the user to update their feedback"""
+
+    if 'username' not in session:
+        return redirect('/login')
+
+    feedback = Feedback.query.get_or_404(id)
+    form = FeedbackForm(obj=feedback)
+
+    if form.validate_on_submit():
+        feedback.title = form.title.data
+        feedback.content = form.content.data
+
+        db.session.commit()
+
+        flash(f"Feedback updated!")
+        return redirect(f"/users/{feedback.username}")
+
+    else:
+        return render_template("feedback.html", form=form)
+
+
+@app.route('/feedback/<int:id>/delete', methods=['POST'])
+def delete_feedback(id):
+    """ Deletes a feedback from the database"""
+
+    if 'username' not in session:
+        return redirect('/login')
+
+    feedback = Feedback.query.get_or_404(id)
+    db.session.delete(feedback)
+    db.session.commit()
+    flash(f"Feedback Deleted!")
+    return redirect(f"/users/{feedback.username}")
+
+
+@app.route('/users/<username>/delete', methods=['POST'])
+def delete_user(username):
+    """ Deletes the user from the database. feedbacks related are also deleted"""
+
+    if 'username' not in session:   
+        return redirect('/login')
+
+    user = User.query.filter_by(username=username).first()
+    db.session.delete(user)
+    db.session.commit()
+    session.pop('username')
+    flash("User Deleted!")
+    return redirect(f"/")
+
+
+    
--- models.py
+++ models.py
@@ -17,8 +17,10 @@ class User(db.Model):
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
-    username = db.Column(db.String(20), nullable=False, primary_key=True, unique=True)
+    username = db.Column(db.String(20), nullable=False, primary_key=True)
     password = db.Column(db.Text, nullable=False)
+
+    feedback = db.relationship('Feedback', backref='user', cascade="all, delete")
 
     @classmethod
     def register(cls, username, password, first_name, last_name, email):
@@ -42,11 +44,12 @@ class User(db.Model):
         else:
             return False
 
+
 class Feedback(db.Model):
     __tablename__ = 'feedbacks'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.Text, nullable=False)
     content = db.Column(db.Text, nullable=False)
-    username = db.Column(db.String, db.ForeignKey('users.username', ondelete='Cascade'))
+    username = db.Column(db.String, db.ForeignKey('users.username', ondelete="CASCADE"), nullable=False)
     
